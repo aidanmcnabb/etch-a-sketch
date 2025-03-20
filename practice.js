23ef23a6e36e5099dbaada0d48eab6943da9fe18
aidanmcnabb <aidan.mcnabb11@gmail.com>
2025-03-19 19:53:28 -0600

    added notes

--- practice.js
+++ practice.js
@@ -1,5 +1,6 @@
-// im going to need an array of objects containing a title and a nested object that contains metadata for each square.
+// im going to need an array of objects containing a title and a nested array that contains metadata for each square.
 // random choice will choose a pair
+// somehow I will have to map the metadata to the box. likely a for loop or for each of the array?
 
 
 // if z-index value is <= 2
@@ -34,6 +35,14 @@ function createCards() {
                 }
                 decrementTranslation()
                 
+                //i will probably have to seperate the outer loop from the inner nested loop so i can only load 2 cards at a time.  if i dont I will have 80*7 square divs and that will probably fuck some computers up. maybe its not that intensive im not sure yet. maybe theres a way to check the location in the DOM tree? like if things are being deleted and added bottom up, maybe i can just use the order of the DOM tree to dictate when boxes should load and then i wouldn't have to take out the nested loop. now that im thinking about it there are a ton of times i would need to manipulate the index of each card in the DOM tree 
+
+                // okay so i need to move all z-indexs' up 1 according to DOM tree loaction.  i also need it to only load two boxes at any given time.  
+                // array.prototype.splice.call()
+                // array.prototype.indexOf()
+                // array.from()
+                // one of these might point me in the right direction to be able to acomplish what im trying to do 
+
                 cardTitle.textContent = 'Drawing!'
                 cardTitle.classList.add('title')
                 card.appendChild(cardTitle)
@@ -70,11 +79,3 @@ createCards()
         //const cardLocation = parseInt(card.style.top)
         //if (currentZIndex <= 7) { 
         //if (currentZIndex <= 2) {
-        //function createSquares() {
-        //}              
-        //createSquares()
-        // }
-        //card.setAttribute('style', `z-index: ${currentZIndex + 1}`)
-        //console.log(currentZIndex)
-        //console.log(cardLocation)
-       // }    
