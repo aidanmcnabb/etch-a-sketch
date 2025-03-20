5c0555a764b96a59f7d6d34f4c90658da91203e6
aidanmcnabb <aidan.mcnabb11@gmail.com>
2025-03-19 19:19:36 -0600

    card animation intro finished

--- practice.js
+++ practice.js
@@ -1,3 +1,6 @@
+// im going to need an array of objects containing a title and a nested object that contains metadata for each square.
+// random choice will choose a pair
+
 
 // if z-index value is <= 2
 
@@ -6,61 +9,59 @@ function createCards() {
     
     // if ((cardnumber) === (arraynumber)) {color = (arraycolor)}
     
-    let cardZIndex = 7
-    let cardTranslationValue = 48
-    
+    let cardZIndex = 1
+    let cardTranslationValue = 55
     let c = 1
-    const cardStartUp = setInterval(() => {
-        if (c <= 7) {
-            const box = document.createElement('div')
-            const cardTitle = document.createElement('header')
-            const card = document.createElement('div')
-            card.classList.add('card')
-            card.classList.add('card-animation')
+    
+    setTimeout(() => {
+        const cardStartUp = setInterval(() => {
+            if (c <= 7) {
+                const box = document.createElement('div')
+                const cardTitle = document.createElement('header')
+                const card = document.createElement('div')
+                card.classList.add('card')
+                card.classList.add('card-animation')
 
-            card.style.zIndex = cardZIndex-- //making each card a lower z-index with each iteration
-            console.log(card.style.zIndex)
-            document.body.appendChild(card)
+                //making each card a lower z-index with each iteration
+                card.style.zIndex = cardZIndex++   
+                console.log(card.style.zIndex)
+                document.body.appendChild(card)
 
-            //changing translation per iteration
-            function decrementTranslation() {
-                console.log(cardTranslationValue)
-                const cardLocation = cardTranslationValue / 100 //this math isnt working i might need to use string
-                card.style.transform = `translate(-50%, ${cardLocation})`
-                cardTranslationValue -= 2
+                //changing translation per iteration
+                function decrementTranslation() {
+                    card.style.transform = `translate(-50%, -${cardTranslationValue}%)`
+                    cardTranslationValue -= 2
+                }
+                decrementTranslation()
                 
-            }
-            decrementTranslation()
+                cardTitle.textContent = 'Drawing!'
+                cardTitle.classList.add('title')
+                card.appendChild(cardTitle)
+                box.classList.add('box')
+                card.appendChild(box)
             
-            cardTitle.textContent = 'Drawing!'
-            cardTitle.classList.add('title')
-            card.appendChild(cardTitle)
-            box.classList.add('box')
-            card.appendChild(box)
-        
-            
-            innerLoop: for (i = 1; i < 100; i++) {
-                const square = document.createElement('div')
-                square.classList.add('square')  //making that squares
-                square.addEventListener('mouseover',  () => {
-                    square.classList.add('mouseover')   //making squares interactable
-                })
-                square.addEventListener('mouseout', () => {
-                    square.classList.remove('mouseover') //
-                })
-                square.addEventListener('click', () => {
-                    square  //will add soon
-                })
-                box.appendChild(square)
                 
-                
-                if (i === 81) {break innerLoop} //breaking innerLoop after quares fill up container+
-            }
-            c++
-        } else {clearInterval(cardStartUp)}
-        
-        
-    }, 120)    
+                innerLoop: for (i = 1; i < 100; i++) {
+                    const square = document.createElement('div')
+                    square.classList.add('square')  //making that squares
+                    square.addEventListener('mouseover',  () => {
+                        square.classList.add('mouseover')   //making squares interactable
+                    })
+                    square.addEventListener('mouseout', () => {
+                        square.classList.remove('mouseover') //
+                    })
+                    square.addEventListener('click', () => {
+                        square  //will add soon
+                    })
+                    box.appendChild(square)
+                    
+                    
+                    if (i === 81) {break innerLoop} //breaking innerLoop after quares fill up container+
+                }
+                c++
+            } else {clearInterval(cardStartUp)}    
+        }, 120)
+    }, 1000)        
     
 }
 createCards()
